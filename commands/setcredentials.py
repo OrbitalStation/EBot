5e014ea716bccb521afd3df5ed6c5bb7d6614dc6
Orbital Station <romann.tarasenko@gmail.com>
2023-06-29 12:40:28 +0600

    Fix minor bugs

--- commands/setcredentials.py
+++ commands/setcredentials.py
@@ -9,18 +9,18 @@ def _vercode(bot, message):
     db.create_table_if_not_exists()
     user = db.create_user_if_not_exists_and_fetch_if_needed(message.from_user.id, do_fetch=True)
     flow = get_flow(bot, message, user.google_disk_client_secrets, const("googleOauth2Scope"))
+    if flow is None:
+        return
     flow.redirect_uri = oauth2client.client.OOB_CALLBACK_URN
     authorize_url = flow.step1_get_authorize_url()
     bot.send_message(message.chat.id, const("botSetGDCredentialsExtraInfo1") + ' ' + authorize_url)
     message = bot.send_message(message.chat.id, const("botSetGDCredentialsExtraInfo2"))
-    bot.register_next_step_handler(message, user_answered(bot, _update, message, None,
+    bot.register_next_step_handler(message, user_answered(bot, _update(bot, message), message, None,
                                                           const("botHumanGDCredentials")))
 
 
-def _update(bot, message, flow):
+def _update(bot, message):
     def update(answer):
-        credentials = flow.step2_exchange()
-
         update_single_field(bot, message, answer.text, "google_disk_credentials", const('botHumanGDCredentials'))
     return update
 
