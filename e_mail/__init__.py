b65c92d9a759b43320754bf103de315d2da17be4
Orbital Station <romann.tarasenko@gmail.com>
2023-06-30 21:55:30 +0600

    Minor fixes; mainly for beauty

--- e_mail/__init__.py
+++ e_mail/__init__.py
@@ -1,1 +1,7 @@
-__all__ = ['send_raw', 'send']
+__all__ = ['send_raw', 'send', 'create_title_for_email_and_attachment']
+
+from convert_time_from_unix import convert
+
+
+def create_title_for_email_and_attachment(time: str):
+    return "Сообщение из Телеграмма, " + time
